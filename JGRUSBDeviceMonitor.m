4089d07959f7d65c46c4d1bf1776399e460a7ee3
Jack Rostron <jackrostron@powa.com>
2014-05-02 14:56:39 +0100

    Initial commit

---
+++ JGRUSBDeviceMonitor.m
@@ -0,0 +1,19 @@
+//
+//  JGRUSBDeviceMonitor.m
+//  RunScriptTest
+//
+//  Created by Jack Rostron on 02/05/2014.
+//  Copyright (c) 2014 Jack Rostron. All rights reserved.
+//
+
+#import "JGRUSBDeviceMonitor.h"
+
+@implementation JGRUSBDeviceMonitor
+
+- (void)monitorForUSBDevicesWithConnectedBlock:(void(^)(NSDictionary *device))connected
+                                  removedBlock:(void(^)(NSDictionary *device))removed
+{
+    
+}
+
+@end
